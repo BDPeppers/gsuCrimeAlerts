0bfb258ba82a10c6403c41816ad389c80718122a
BDPeppers <peppersbre@yahoo.com>
2019-04-14 23:10:51 -0400

    mobile view

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+//viewing
+//view variable
+$('#2d').click(function(){
+    $(this).css("background-color", "red");
+    $(this).css("color", "white");
+    //set view vairable
+
+    $('#3d').css("background-color", "white");
+    $('#3d').css("color", "red")
+    
+})
+
+$('#3d').click(function(){
+    $(this).css("background-color", "red");
+    $(this).css("color", "white");
+
+    $('#2d').css("background-color", "white");
+    $('#2d').css("color", "red")
+    
+})
+
+import {map, TwoD} from module
+
+
+
